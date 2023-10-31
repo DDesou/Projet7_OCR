a6d9b50ecadb1a4afd01bd02b8e4fb6ece1fea35
DDesou <140590021+DDesou@users.noreply.github.com>
2023-10-31 02:11:46 +0100

    20231030 Ppt updated

---
+++ Desoubzdanne_Denis_1_API_112023/test_main.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+from main import app
+
+client = TestClient(app=app)
+
+def test_index_returns_correct():
+    response = client.get('/')
+
+    assert response.status_code == status.HTTP_200_ok
+    assert response.json() == {'message' : 'my API works!!'}
